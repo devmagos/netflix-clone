6b0ba3b84a88e26ecfc5c26534085fbc380bcbb6
MAC <mac@MACs-MacBook.local>
2021-05-31 12:54:52 +0100

    restore banner description width

--- src/Banner.js
+++ src/Banner.js
@@ -32,7 +32,7 @@ function Banner() {
                 <button className="banner__button">Play</button>
                 <button className="banner__button">My List</button>
             </div>
-            <h1 className="banner_description">{truncate(movie?.overview, 150)}</h1>
+            <h1 className="banner__description">{truncate(movie?.overview, 150)}</h1>
             </div>
 
             <div className="banner--fadeBottom" />
